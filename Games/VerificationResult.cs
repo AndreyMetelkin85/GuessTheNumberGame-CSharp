0e9a7ad56619ebb0b6a5d83fb61113dfb1b6aef6
Andrey <80163631+AndreyMetelkin85@users.noreply.github.com>
2024-03-05 21:33:38 +0300

    Fix count

--- Games/VerificationResult.cs
+++ Games/VerificationResult.cs
@@ -6,28 +6,24 @@ public class VerificationResult
     {
         var playerData = new Player();
         var playerName = playerData.Name();
-        var userNumber = playerData.UserNumberEntry();
         var number = new DataGeneration();
         var numberGenerator = number.GaneratorNuber();
 
         var count = 0;
         while (true)
         {
+            var userNumber = playerData.UserNumberEntry();
+            count++;
+
             if (userNumber < numberGenerator)
             {
                 Console.WriteLine(
                     $"{playerName}, твоё число {userNumber} и оно меньше, чем задумал компьютер. Пробуй ещё раз.");
-                Console.Write($"{playerName}, введи новое число : ");
-                userNumber = int.Parse(Console.ReadLine());
-                count++;
             }
             else if (userNumber > numberGenerator)
             {
                 Console.WriteLine(
                     $"{playerName}, твоё число {userNumber} и оно больше, чем задумал компьютер. Пробуй ещё раз.");
-                Console.Write($"{playerName}, введи новое число : ");
-                userNumber = int.Parse(Console.ReadLine());
-                count++;
             }
             else
             {
